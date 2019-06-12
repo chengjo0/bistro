f9727cd643ca09d2a4ee0ff799e183fb7ef44c1e
chengjo0 <cheng.john@pm.me>
2019-06-13 00:57:36 +0300

    added typestyle and made a fixed header with changing opacity

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,61 @@
 import * as React from 'react'
+import { style } from 'typestyle'
+import { rem, px, percent, color } from 'csx'
+import { Link } from 'gatsby'
+import * as csstips from 'csstips'
 
-export default () => <div />
+const bg = color('rgb(255, 255, 24)')
+
+const Header = () => {
+  const [headerOpacity, setHeaderOpacity] = React.useState(0)
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setHeaderOpacity(window.pageYOffset / 300)
+    })
+  })
+
+  return (
+    <header
+      className={style({
+        background: bg.fade(headerOpacity).toString(),
+        height: rem(6),
+        position: 'fixed',
+        top: px(0),
+        width: percent(100),
+      })}
+    >
+      <div
+        className={style({
+          ...csstips.horizontal,
+          ...csstips.center,
+          ...csstips.betweenJustified,
+          ...csstips.padding(rem(0), rem(16)),
+          height: rem(5),
+        })}
+      >
+        <Link to="/">
+          <h3>Bistro d'Asie</h3>
+        </Link>
+        <div
+          className={style({
+            ...csstips.horizontal,
+            ...csstips.horizontallySpaced(rem(2)),
+          })}
+        >
+          <Link to="/">
+            <h3>Home</h3>
+          </Link>
+          <Link to="/about/">
+            <h3>About</h3>
+          </Link>
+          <Link to="/contact/">
+            <h3>Contact</h3>
+          </Link>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,52 +1,17 @@
+import { rem } from 'csx'
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Header from './header'
 
 interface Props {
   children: React.ReactNode
 }
 
-const ListLink = (props: { to: string; children: string }) => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-    <Link to={props.to}>{props.children}</Link>
-  </li>
-)
-
 export default (props: Props) => (
   <div>
-    <header
-      style={{
-        padding: `0rem 16rem`,
-        backgroundColor: 'red',
-        height: `6rem`,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: `space-between`,
-      }}
-    >
-      <Link to="/">
-        <h3>Bistro d'Asie</h3>
-      </Link>
-      <div
-        style={{
-          flexDirection: 'row',
-          display: 'flex',
-        }}
-      >
-        <Link to="/">
-          <h3>Home</h3>
-        </Link>
-        <Link to="/about/">
-          <h3>About</h3>
-        </Link>
-        <Link to="/contact/">
-          <h3>Contact</h3>
-        </Link>
-      </div>
-    </header>
+    <Header />
     <div
       style={{
-        margin: `3rem 16rem`,
-        maxWidth: `70%`,
+        margin: `${rem(8)} ${rem(16)}`,
       }}
     >
       {props.children}
