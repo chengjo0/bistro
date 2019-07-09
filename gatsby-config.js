ef2ba6fb699eaf91a63e73f1b3e4b357b7fb2965
chengjo0 <cheng.john@pm.me>
2019-07-10 01:01:02 +0300

    switch to yarn and contentful integrated

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,35 +1,14 @@
-let contentfulConfig
-
-try {
-  // Load the Contentful config from the .contentful.json
-  contentfulConfig = require('./.contentful')
-} catch (_) {}
-
-// Overwrite the Contentful config with environment variables if they exist
-contentfulConfig = {
-  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
-  accessToken:
-    process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
-}
-
-// console.log(JSON.stringify(contentfulConfig, undefined, 2))
-
-const { spaceId, accessToken } = contentfulConfig
-
-if (!spaceId || !accessToken) {
-  throw new Error(
-    'Contentful spaceId and the delivery token need to be provided.'
-  )
-}
-
 module.exports = {
   plugins: [
-    {
-      resolve: 'gatsby-source-contentful',
-      options: contentfulConfig,
-    },
     `gatsby-plugin-typescript`,
     'gatsby-plugin-react-helmet',
     'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `uvtt2rezrutz`,
+        accessToken: `5qk4G2T8-DJKx8EzlLOie0QUsST0lARHqDSXrOuZL7U`,
+      },
+    },
   ],
 }
