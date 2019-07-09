22ab2ba9bdff1ced5372eb741e1c14ce710e6919
chengjo0 <cheng.john@pm.me>
2019-07-09 23:01:56 +0300

    trying to make contentful work

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,35 @@
+let contentfulConfig
+
+try {
+  // Load the Contentful config from the .contentful.json
+  contentfulConfig = require('./.contentful')
+} catch (_) {}
+
+// Overwrite the Contentful config with environment variables if they exist
+contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID || contentfulConfig.spaceId,
+  accessToken:
+    process.env.CONTENTFUL_DELIVERY_TOKEN || contentfulConfig.accessToken,
+}
+
+// console.log(JSON.stringify(contentfulConfig, undefined, 2))
+
+const { spaceId, accessToken } = contentfulConfig
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    'Contentful spaceId and the delivery token need to be provided.'
+  )
+}
+
 module.exports = {
   plugins: [
-    `gatsby-plugin-typescript`,
     {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
+      resolve: 'gatsby-source-contentful',
+      options: contentfulConfig,
     },
+    `gatsby-plugin-typescript`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-remark',
   ],
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,144 +1,168 @@
+import * as contentful from 'contentful'
 import * as csstips from 'csstips'
-import { percent, rem, px } from 'csx'
+import { percent, px, rem } from 'csx'
 import * as React from 'react'
 import { style } from 'typestyle'
 import Layout from '../components/layout'
 
-export default () => (
-  <Layout>
-    <img
-      src="hero.jpg"
-      alt="hero"
-      className={style({
-        gridColumn: '1 / 4',
-        maxHeight: rem(50),
-        width: percent(100),
-      })}
-    />
-    <div
-      className={style({
-        gridColumn: '2 / 3',
-        gridRowStart: '2',
-        textAlign: 'justify',
-        paddingBottom: rem(2),
-        ...csstips.verticallySpaced(rem(2)),
-      })}
-    >
+const SPACE_ID = 'uvtt2rezrutz'
+const ACCESS_TOKEN = '5qk4G2T8-DJKx8EzlLOie0QUsST0lARHqDSXrOuZL7U'
+
+const client = contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: SPACE_ID,
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: ACCESS_TOKEN,
+})
+
+export default () => {
+  React.useEffect(() => {
+    async function fetchData() {
+      const result = await client.getEntries({
+        content_type: `dish`,
+      })
+      console.log(result.items)
+    }
+
+    fetchData()
+  })
+
+  return (
+    <Layout>
+      <img
+        src="hero.jpg"
+        alt="hero"
+        className={style({
+          gridColumn: '1 / 4',
+          maxHeight: rem(50),
+          width: percent(100),
+        })}
+      />
       <div
         className={style({
-          ...csstips.vertical,
-          $nest: {
-            '&>h1': {
-              marginBottom: rem(1),
-            },
-            '&>div>span': {
-              fontWeight: 'bold',
-            },
-            '&>div:last-child': {
-              paddingTop: rem(0.5),
-            },
-          },
+          gridColumn: '2 / 3',
+          gridRowStart: '2',
+          textAlign: 'justify',
+          paddingBottom: rem(2),
+          ...csstips.verticallySpaced(rem(2)),
         })}
       >
-        <h1>Opening hours</h1>
-        <div>
-          <span>Tues - Sat :</span> 11:30 - 14:00 / 17:30 - 21:00
-        </div>
-        <div>
-          <span>Sun :</span> 11:30 - 14:00
-        </div>
-        <div>
-          <span>Mon :</span> Closed
-        </div>
-        <div>
-          <div>+33 4 50 70 10 48</div>
-          <div>1A avenue du Leman, 74200 Thonon-les-Bains</div>
-        </div>
-      </div>
-      <div
-        className={style({
-          display: 'grid',
-          gridTemplateRows: `75% 25%`,
-          height: rem(15),
-        })}
-      >
-        <img
-          src="hero.jpg"
-          alt="hero"
-          className={style({
-            height: percent(100),
-            width: percent(100),
-          })}
-        />
         <div
           className={style({
-            ...csstips.horizontal,
-            ...csstips.centerCenter,
-            borderWidth: `${px(0)} ${px(2)} ${px(2)} ${px(2)}`,
-            borderStyle: 'solid',
-            borderBottomLeftRadius: px(3),
-            borderBottomRightRadius: px(3),
+            ...csstips.vertical,
+            $nest: {
+              '&>h1': {
+                marginBottom: rem(1),
+              },
+              '&>div>span': {
+                fontWeight: 'bold',
+              },
+              '&>div:last-child': {
+                paddingTop: rem(0.5),
+              },
+            },
           })}
         >
-          Menu
+          <h1>Opening hours</h1>
+          <div>
+            <span>Tues - Sat :</span> 11:30 - 14:00 / 17:30 - 21:00
+          </div>
+          <div>
+            <span>Sun :</span> 11:30 - 14:00
+          </div>
+          <div>
+            <span>Mon :</span> Closed
+          </div>
+          <div>
+            <div>+33 4 50 70 10 48</div>
+            <div>1A avenue du Leman, 74200 Thonon-les-Bains</div>
+          </div>
         </div>
-      </div>
-      <div
-        className={style({
-          display: 'grid',
-          gridTemplateRows: `75% 25%`,
-          height: rem(15),
-        })}
-      >
-        <img
-          src="hero.jpg"
-          alt="hero"
-          className={style({
-            height: percent(100),
-            width: percent(100),
-          })}
-        />
         <div
           className={style({
-            ...csstips.horizontal,
-            ...csstips.centerCenter,
-            borderWidth: `${px(0)} ${px(2)} ${px(2)} ${px(2)}`,
-            borderStyle: 'solid',
-            borderBottomLeftRadius: px(3),
-            borderBottomRightRadius: px(3),
+            display: 'grid',
+            gridTemplateRows: `75% 25%`,
+            height: rem(15),
           })}
         >
-          Starters
+          <img
+            src="hero.jpg"
+            alt="hero"
+            className={style({
+              height: percent(100),
+              width: percent(100),
+            })}
+          />
+          <div
+            className={style({
+              ...csstips.horizontal,
+              ...csstips.centerCenter,
+              borderWidth: `${px(0)} ${px(2)} ${px(2)} ${px(2)}`,
+              borderStyle: 'solid',
+              borderBottomLeftRadius: px(3),
+              borderBottomRightRadius: px(3),
+            })}
+          >
+            Menu
+          </div>
         </div>
-      </div>
-      <div
-        className={style({
-          display: 'grid',
-          gridTemplateRows: `75% 25%`,
-          height: rem(15),
-        })}
-      >
-        <img
-          src="hero.jpg"
-          alt="hero"
-          className={style({
-            height: percent(100),
-            width: percent(100),
-          })}
-        />
         <div
           className={style({
-            ...csstips.horizontal,
-            ...csstips.centerCenter,
-            borderWidth: `${px(0)} ${px(2)} ${px(2)} ${px(2)}`,
-            borderStyle: 'solid',
-            borderBottomLeftRadius: px(3),
-            borderBottomRightRadius: px(3),
+            display: 'grid',
+            gridTemplateRows: `75% 25%`,
+            height: rem(15),
           })}
         >
-          Desserts
+          <img
+            src="hero.jpg"
+            alt="hero"
+            className={style({
+              height: percent(100),
+              width: percent(100),
+            })}
+          />
+          <div
+            className={style({
+              ...csstips.horizontal,
+              ...csstips.centerCenter,
+              borderWidth: `${px(0)} ${px(2)} ${px(2)} ${px(2)}`,
+              borderStyle: 'solid',
+              borderBottomLeftRadius: px(3),
+              borderBottomRightRadius: px(3),
+            })}
+          >
+            Starters
+          </div>
+        </div>
+        <div
+          className={style({
+            display: 'grid',
+            gridTemplateRows: `75% 25%`,
+            height: rem(15),
+          })}
+        >
+          <img
+            src="hero.jpg"
+            alt="hero"
+            className={style({
+              height: percent(100),
+              width: percent(100),
+            })}
+          />
+          <div
+            className={style({
+              ...csstips.horizontal,
+              ...csstips.centerCenter,
+              borderWidth: `${px(0)} ${px(2)} ${px(2)} ${px(2)}`,
+              borderStyle: 'solid',
+              borderBottomLeftRadius: px(3),
+              borderBottomRightRadius: px(3),
+            })}
+          >
+            Desserts
+          </div>
         </div>
       </div>
-    </div>
-  </Layout>
-)
+    </Layout>
+  )
+}
