1a1d68f084d54f1d05c30550fcf20b4902e30a0f
chengjo0 <cheng.john@pm.me>
2019-07-10 11:35:11 +0300

    switched to env vars and added i18n

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
+const activeEnv =
+  process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || 'development'
+
+console.log(`Using environment config: '${activeEnv}'`)
+
+require('dotenv').config({
+  path: `.env.${activeEnv}`,
+})
+
 module.exports = {
   plugins: [
     `gatsby-plugin-typescript`,
@@ -6,8 +15,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `uvtt2rezrutz`,
-        accessToken: `5qk4G2T8-DJKx8EzlLOie0QUsST0lARHqDSXrOuZL7U`,
+        spaceId: process.env.GATSBY_SPACE_ID,
+        accessToken: process.env.GATSBY_ACCESS_TOKEN,
       },
     },
   ],
