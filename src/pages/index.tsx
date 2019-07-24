2edbf2d7defdb8e003d8a28a616cba0f38240f79
chengjo0 <cheng.john@pm.me>
2019-07-24 15:56:10 +0300

    removed unused code and passes props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
 import * as csstips from 'csstips'
-import { deg, px, rem, rotate } from 'csx'
+import { percent, px, rem } from 'csx'
 import { graphql } from 'gatsby'
 import * as React from 'react'
-import Helmet from 'react-helmet'
-import { keyframes, style } from 'typestyle'
+import { classes, keyframes, style } from 'typestyle'
 import Layout from '../components/layout'
 
 interface Props {
@@ -59,222 +58,70 @@ const pulse = keyframes({
 
 export default function Home({ data }: Props) {
   return (
-    <Layout>
-      <Helmet>
-        <title>
-          {data.allContentfulInformations.edges[0].node.name} | Welcome
-        </title>
-        <link rel="stylesheet" href="animate.min.css"></link>
-      </Helmet>
+    <Layout pageName="Welcome">
       <div
         className={style({
-          ...csstips.vertical,
-          ...csstips.centerCenter,
-          gridColumn: '2 / 3',
-          textAlign: 'justify',
-          marginTop: rem(3),
-          paddingBottom: rem(2),
-        })}
-      >
-        <div
-          className={style({
-            ...csstips.flex,
-            ...csstips.centerCenter,
-            animationName: keyframes({
-              '0%': { opacity: 0 },
-              '100%': { opacity: 1 },
-            }),
-            animationDuration: '1s',
-            animationTimingFunction: 'ease-in',
-          })}
-        >
-          Welcome
-        </div>
-        <span
-          className={style({
-            animationName: pulse,
-            animationDuration: '3s',
-            animationIterationCount: 'infinite',
-            animationTimingFunction: 'ease-in',
-            position: 'relative',
-            $nest: {
-              '&::before': {
-                content: '""',
-                display: 'block',
-                position: 'absolute',
-                top: rem(-2),
-                left: rem(-1.1),
-                height: rem(3),
-                width: px(3),
-                borderRadius: px(2),
-                backgroundColor: 'black',
-                transform: rotate(deg(-45)),
-              },
-              '&::after': {
-                content: '""',
-                display: 'block',
-                position: 'absolute',
-                top: rem(-2),
-                right: rem(-1.1),
-                height: rem(3),
-                width: px(3),
-                borderRadius: px(2),
-                backgroundColor: 'black',
-                transform: rotate(deg(45)),
-              },
+          height: percent(100),
+          display: 'grid',
+          gridTemplateColumns: `${px(25)} auto ${px(25)}`,
+          $nest: {
+            '@media screen and (min-width: 500px)': {
+              gridTemplateColumns: `${px(250)} auto ${px(250)}`,
             },
-          })}
-        ></span>
-      </div>
-      <div
-        className={style({
-          gridColumn: '2 / 3',
-          backgroundColor: 'red',
-        })}
-      >
-        and then...
-      </div>
-      {/* <img
-        src={String(data.contentfulAsset.fluid.src)}
-        alt="hero"
-        className={style({
-          gridColumn: '1 / 4',
-          maxHeight: rem(50),
-          width: percent(100),
-        })}
-      />
-      <div
-        className={style({
-          gridColumn: '2 / 3',
-          gridRowStart: '2',
-          textAlign: 'justify',
-          paddingBottom: rem(2),
-          ...csstips.verticallySpaced(rem(2)),
+          },
         })}
       >
         <div
           className={style({
             ...csstips.vertical,
-            $nest: {
-              '&>h1': {
-                marginBottom: rem(1),
-              },
-              '&>div>span': {
-                fontWeight: 'bold',
-              },
-              '&>div:last-child': {
-                paddingTop: rem(0.5),
-              },
-            },
+            ...csstips.centerCenter,
+            gridColumn: '2 / 3',
+            textAlign: 'justify',
+            marginTop: rem(3),
+            paddingBottom: rem(2),
+            position: 'relative',
           })}
         >
-          <h1>Horaires:</h1>
-          {data.allContentfulHorairesDouverture.edges[0].node.openingHour.map(
-            openingHours => (
-              <div key={String(openingHours.period)}>
-                <span>{openingHours.period}: </span>
-                {openingHours.isOpenForLunch
-                  ? `${openingHours.lunchOpeningTime} - ${
-                      openingHours.lunchClosingTime
-                    } ${
-                      openingHours.isOpenForDinner
-                        ? ` / ${openingHours.dinnerOpeningTime} - ${openingHours.dinnerClosingTime}`
-                        : ''
-                    }`
-                  : `fermé`}
-              </div>
-            )
-          )}
-          <div>
-            <div>{data.allContentfulInformations.edges[0].node.phone}</div>
-            <div>{data.allContentfulInformations.edges[0].node.address}</div>
-          </div>
-        </div>
-        <div
-          className={style({
-            display: 'grid',
-            gridTemplateRows: `75% 25%`,
-            height: rem(15),
-          })}
-        >
-          <img
-            src={String(data.allContentfulPlat.edges[0].node.picture.fluid.src)}
-            alt="hero"
-            className={style({
-              height: percent(100),
-              width: percent(100),
-            })}
-          />
           <div
             className={style({
-              ...csstips.horizontal,
+              ...csstips.flex,
+              ...csstips.vertical,
               ...csstips.centerCenter,
-              borderWidth: `${px(0)} ${px(2)} ${px(2)} ${px(2)}`,
-              borderStyle: 'solid',
-              borderBottomLeftRadius: px(3),
-              borderBottomRightRadius: px(3),
+              animationName: keyframes({
+                '0%': { opacity: 0 },
+                '100%': { opacity: 1 },
+              }),
+              animationDuration: '1s',
+              animationTimingFunction: 'ease-in',
+            })}
+          ></div>
+          <span
+            className={style({
+              animationName: pulse,
+              animationDuration: '3s',
+              animationIterationCount: 'infinite',
+              animationTimingFunction: 'ease-in',
             })}
           >
-            Menu
-          </div>
+            <i
+              className={classes(
+                'fas fa-angle-double-down',
+                style({
+                  fontSize: rem(2),
+                })
+              )}
+            ></i>
+          </span>
         </div>
-        <div
-          className={style({
-            display: 'grid',
-            gridTemplateRows: `75% 25%`,
-            height: rem(15),
-          })}
-        >
-          <img
-            src={String(data.allContentfulPlat.edges[0].node.picture.fluid.src)}
-            alt="hero"
-            className={style({
-              height: percent(100),
-              width: percent(100),
-            })}
-          />
-          <div
-            className={style({
-              ...csstips.horizontal,
-              ...csstips.centerCenter,
-              borderWidth: `${px(0)} ${px(2)} ${px(2)} ${px(2)}`,
-              borderStyle: 'solid',
-              borderBottomLeftRadius: px(3),
-              borderBottomRightRadius: px(3),
-            })}
-          >
-            Starters
-          </div>
-        </div>
-        <div
-          className={style({
-            display: 'grid',
-            gridTemplateRows: `75% 25%`,
-            height: rem(15),
-          })}
-        >
-          <img
-            src="hero.jpg"
-            alt="hero"
-            className={style({
-              height: percent(100),
-              width: percent(100),
-            })}
-          />
-          <div
-            className={style({
-              ...csstips.horizontal,
-              ...csstips.centerCenter,
-              borderWidth: `${px(0)} ${px(2)} ${px(2)} ${px(2)}`,
-              borderStyle: 'solid',
-              borderBottomLeftRadius: px(3),
-              borderBottomRightRadius: px(3),
-            })}
-          >
-            Desserts
-          </div>
-        </div>
-      </div> */}
+      </div>
+      <div
+        className={style({
+          backgroundColor: 'red',
+          height: percent(100),
+        })}
+      >
+        and then...
+      </div>
     </Layout>
   )
 }
