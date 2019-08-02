36b57a1aba3df41511ee748f5a01400a025c02df
chengjo0 <cheng.john@pm.me>
2019-08-02 11:28:15 +0300

    updated 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,13 @@
+import * as csstips from 'csstips'
 import * as React from 'react'
+import Layout from '../components/layout'
+import { style } from 'typestyle'
+import { percent } from 'csx'
 
-export default function() {
-  return <div>404, you got lost!!!</div>
-}
+export default () => (
+  <Layout pageName="404" withPadding>
+    <div className={style({ ...csstips.centerCenter, height: percent(100) })}>
+      404, you got lost...
+    </div>
+  </Layout>
+)
