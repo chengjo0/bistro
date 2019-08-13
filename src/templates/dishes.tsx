d3c7cf572b6c3bd0e14b99ebebb72f950592b88f
chengjo0 <cheng.john@pm.me>
2019-08-13 13:45:29 +0300

    Bo-buns are shown

--- src/templates/dishes.tsx
+++ src/templates/dishes.tsx
@@ -23,146 +23,157 @@ interface Props {
   pageContext: {
     pageName: String
     accompaniementMessage: String
+    dishesByCategories: Array<{
+      title: String
+      dishes: Array<{
+        name: String
+        description: String
+        price: Number
+        spicy: Boolean
+      }>
+    }>
   }
 }
 
-export default ({ data, pageContext }: Props) => (
-  <Layout pageName={pageContext.pageName} withPadding>
-    <div
-      className={style(
-        ...Theme.breakpoints({
-          mobile: {
-            padding: Theme.paddings.mobile,
-          },
-          desktop: {
-            padding: Theme.paddings.desktop,
-          },
-        }),
-        {
-          ...csstips.flex,
-          ...csstips.vertical,
-          ...csstips.verticallySpaced(rem(1)),
-          $nest: {
-            '& > div:not(:first-child)': {
-              paddingTop: rem(3),
-            },
-          },
-        }
-      )}
-    >
+export default ({ pageContext }: Props) => {
+  return (
+    <Layout pageName={pageContext.pageName} withPadding>
       <div
-        className={style({
-          ...csstips.horizontal,
-          ...csstips.horizontallySpaced(rem(1)),
-        })}
+        className={style(
+          ...Theme.breakpoints({
+            mobile: {
+              padding: Theme.paddings.mobile,
+            },
+            desktop: {
+              padding: Theme.paddings.desktop,
+            },
+          }),
+          {
+            ...csstips.flex,
+            ...csstips.vertical,
+            ...csstips.verticallySpaced(rem(1)),
+            $nest: {
+              '& > div:not(:first-child)': {
+                paddingTop: rem(3),
+              },
+            },
+          }
+        )}
       >
         <div
           className={style({
-            ...csstips.content,
-            ...csstips.vertical,
-            ...csstips.centerCenter,
-          })}
-        >
-          <i
-            className={classes(
-              'fas fa-star',
-              style({
-                color: Theme.colors.gold.toString(),
-              })
-            )}
-          ></i>
-        </div>
-        <div
-          className={style({
-            ...csstips.flex,
             ...csstips.horizontal,
-            fontSize: Theme.fontSizes.textExtraSmall,
-            fontStyle: 'italic',
-          })}
-        >
-          {pageContext.accompaniementMessage}
-        </div>
-      </div>
-      {data.contentfulPlats.category.map(category => (
-        <div
-          key={String(category.title)}
-          className={style({
-            ...csstips.centerCenter,
-            ...csstips.vertical,
+            ...csstips.horizontallySpaced(rem(1)),
           })}
         >
           <div
-            className={classes(
-              Theme.styles.title,
-              style({
-                paddingBottom: rem(3),
-                ...csstips.vertical,
-                ...csstips.center,
-                ...csstips.verticallySpaced(rem(1)),
-                textAlign: 'center',
-              })
-            )}
+            className={style({
+              ...csstips.content,
+              ...csstips.vertical,
+              ...csstips.centerCenter,
+            })}
           >
-            <span>{category.title}</span>
-            <span
-              className={style({
-                borderBottomStyle: 'solid',
-                borderBottomWidth: px(1),
-                borderBottomColor: Theme.colors.gold.toString(),
-                width: rem(3),
-              })}
-            ></span>
+            <i
+              className={classes(
+                'fas fa-star',
+                style({
+                  color: Theme.colors.gold.toString(),
+                })
+              )}
+            ></i>
           </div>
-          {category.dishes.map((dish, index) => (
+          <div
+            className={style({
+              ...csstips.flex,
+              ...csstips.horizontal,
+              fontSize: Theme.fontSizes.textExtraSmall,
+              fontStyle: 'italic',
+            })}
+          >
+            {pageContext.accompaniementMessage}
+          </div>
+        </div>
+        {pageContext.dishesByCategories.map(category => (
+          <div
+            key={String(category.title)}
+            className={style({
+              ...csstips.centerCenter,
+              ...csstips.vertical,
+            })}
+          >
             <div
-              key={String(dish.name)}
-              className={style({
-                ...csstips.vertical,
-                ...csstips.verticallySpaced(rem(1)),
-                textAlign: 'center',
-                paddingTop: index > 0 ? rem(2) : rem(0),
-              })}
+              className={classes(
+                Theme.styles.title,
+                style({
+                  paddingBottom: rem(3),
+                  ...csstips.vertical,
+                  ...csstips.center,
+                  ...csstips.verticallySpaced(rem(1)),
+                  textAlign: 'center',
+                })
+              )}
             >
+              <span>{category.title}</span>
               <span
                 className={style({
-                  fontFamily: Theme.fonts.title,
-                  fontSize: Theme.fontSizes.textMedium,
-                  fontWeight: 400,
+                  borderBottomStyle: 'solid',
+                  borderBottomWidth: px(1),
+                  borderBottomColor: Theme.colors.gold.toString(),
+                  width: rem(3),
+                })}
+              ></span>
+            </div>
+            {category.dishes.map((dish, index) => (
+              <div
+                key={String(dish.name)}
+                className={style({
+                  ...csstips.vertical,
+                  ...csstips.verticallySpaced(rem(1)),
+                  textAlign: 'center',
+                  paddingTop: index > 0 ? rem(2) : rem(0),
                 })}
               >
-                {dish.name}
-                {dish.spicy ? (
-                  <i
-                    className={classes(
-                      'fas fa-pepper-hot',
-                      style({
-                        paddingLeft: rem(1),
-                        color: color('red')
-                          .darken(0.1)
-                          .toString(),
-                      })
-                    )}
-                  ></i>
-                ) : null}
-              </span>
-              {dish.description ? (
                 <span
                   className={style({
-                    fontSize: Theme.fontSizes.textExtraSmall,
-                    fontStyle: 'italic',
+                    fontFamily: Theme.fonts.title,
+                    fontSize: Theme.fontSizes.textMedium,
+                    fontWeight: 400,
                   })}
                 >
-                  ( {dish.description} )
+                  {dish.name}
+                  {dish.spicy ? (
+                    <i
+                      className={classes(
+                        'fas fa-pepper-hot',
+                        style({
+                          paddingLeft: rem(1),
+                          color: color('red')
+                            .darken(0.1)
+                            .toString(),
+                        })
+                      )}
+                    ></i>
+                  ) : null}
                 </span>
-              ) : null}
-              <span>{dish.price}0 €</span>
-            </div>
-          ))}
-        </div>
-      ))}
-    </div>
-  </Layout>
-)
+                {dish.description ? (
+                  <span
+                    className={style({
+                      fontSize: Theme.fontSizes.textExtraSmall,
+                      fontStyle: 'italic',
+                    })}
+                  >
+                    ( {dish.description} )
+                  </span>
+                ) : null}
+                <span>{dish.price}0 €</span>
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query GetDishesPages($locale: String) {
